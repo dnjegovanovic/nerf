b076e2d22a94cb009ac474c533261ecbca16433e
dusannj <dusan.njegovnaovic95@gmail.com>
2024-04-20 22:29:14 +0200

    remove all files.

--- tests/test_lego_data.py
+++
@@ -1,7 +0,0 @@
-from nerf_model.dataset.lego_dataset import LegoDataset
-from nerf_model.config.core import config
-
-
-def test_lego_dataset_loading():
-    lego_dataset = LegoDataset(config.model_config.data_dir, debug=True)
-    assert lego_dataset.images.shape[0] == lego_dataset.poses.shape[0]
